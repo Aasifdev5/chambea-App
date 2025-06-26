4e3120f1054710f760ad87a3abb1b1c29db8bc2e
Aasif Ahmed <asifarslaan@gmail.com>
2025-06-26 20:48:18 +0530

    26
    26

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -19,24 +16,20 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.chambea"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,4 +37,30 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
+
+    implementation("com.google.firebase:firebase-auth") {
+        exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-savedstate")
+    }
+    implementation("com.google.firebase:firebase-firestore") {
+        exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-savedstate")
+    }
+    implementation("com.google.firebase:firebase-storage") {
+        exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-savedstate")
+    }
+    implementation("com.google.firebase:firebase-appcheck") {
+        exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-savedstate")
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("androidx.lifecycle:lifecycle-viewmodel:2.8.7")
+        force("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
+    }
 }
